11f429415e2027058906968f1590eac389f6237a
David Dever <david.dever@cox.net>
2014-10-31 17:20:28 -0700

    Legends.xml parsing

--- app/jobs/xml_import_job.rb
+++ app/jobs/xml_import_job.rb
@@ -2,6 +2,10 @@ require 'saxerator'
 
 parser = Saxerator.parser(File.new("/home/david/df/app/dfdata/region2-00005-01-01-legends.xml"))
 
-puts parser.within('region').map.first { |item| item }
+region = parser.within('regions').map { |item| item }
 
-  
+underground_region = parser.within("underground_regions").map { |item| item}
+
+site = parser.within("sites").map { |item| item}
+
+artifact = parser.within("artifacts").map { |item| item}
