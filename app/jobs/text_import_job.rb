f0b2dedec0aa348a6b7e28f89e57e1f3011fedbc
David Dever <david.dever@cox.net>
2014-11-07 19:29:38 -0800

    Regex and syntax fixes in text parser

--- app/jobs/text_import_job.rb
+++ app/jobs/text_import_job.rb
@@ -1,20 +1,32 @@
 race_total = {}
+total_pop = ""
+section = ""
+
 
 File.open("/home/david/df/app/dfdata/region2-00005-01-01-world_sites_and_pops.txt").readlines.each do |line|
 
-  section = "Totals" if line == "Civilized World Population"
+  line = line.encode('UTF-8')
+
+  if !section.nil?
+    section = "Totals" if line.chomp == "Civilized World Population"
+    section = "Sites" if line.chomp == "Sites"
+    section = "Outdoor" if line.chomp == "Outdoor Animal Populations (Including Undead)"
+    section = "Underground" if line.chomp == "Outdoor Animal Populations (Including Undead)"
+  end
 
   if section == "Totals"
-    if line.match(/\s\b(.+?)\b/)
-      race_total = { line.match(/[A-Z](.+?)\b/) => line.match(/\d/) }
+    if /\b(\d+?)\s\b[A-Z]([a-z]+?)\b/.match(line)
+      race_total.store(/[A-Z](.+?)\b/.match(line), /(\d+?)\b/.match(line))
     elsif
-      if line.match(/\s\bTotal:\s(\d+?)\b/)
-        total_pop = line.match(/\d/)
+      if /\bTotal:\s(\d+?)\b/.match(line)
+        total_pop = /(\d+?)\b/.match(line)
       end
     end
   end
 end
 
+race_total.each { |key, value| puts "#{value} #{key}" }
+puts total_pop
 
 #  if line.match(\(.+?):\s(.+?),\s"(.+?)",\s(.+?)\b\)
 
