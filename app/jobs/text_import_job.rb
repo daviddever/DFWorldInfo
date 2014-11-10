a684fcea9e9c6798e79d4100e94fff239c96bc19
David Dever <david.dever@cox.net>
2014-11-10 14:02:13 -0800

    Text parser site section

--- app/jobs/text_import_job.rb
+++ app/jobs/text_import_job.rb
@@ -2,7 +2,9 @@ race_total = {}
 total_pop = ""
 section = ""
 site_name = Array.new
-
+site_owner = {}
+parent_civ = {}
+site_race_total = {}
 
 File.open("/home/david/df/app/dfdata/region2-00005-01-01-world_sites_and_pops.txt").readlines.each do |line|
 
@@ -29,15 +31,20 @@ File.open("/home/david/df/app/dfdata/region2-00005-01-01-world_sites_and_pops.tx
     if /(\d+?):\s(.+?),\s"(.+?)",\s(.+?)\z/.match(line)
       site_name.push(/:\s\b(.+?),/.match(line))
       site_number = /(\d+?):/.match(line)
+    elsif /Owner:\s(.+?),\s(.+?)\z/.match(line)
+      site_owner.store(site_number, /\s((\w+?)\b+?)/.match(line))
+    elsif /Parent\sCiv:\s(.+?)\z/.match(line)
+      parent_civ.store(site, /Parent\sCiv:\s(.+?)\z/.match(line))
+    elsif
+      if /\b(\d+?)\s\b[A-Z]([a-z]+?)\b/.match(line)
+        site_race_total.store(/[A-Z](.+?)\b/.match(line), /(\d+?)\b/.match(line))
+      end
     end
-#    if /Owner:\s(.+?),\s(.+?)\z/.match(line)
-#      site_owner
   end
 end
 
 race_total.each { |key, value| puts "#{value} #{key}" }
 puts "Total Population: #{total_pop}"
 
-
-
+site_owner.each { |key, value| puts "#{key} #{value}" }
 
