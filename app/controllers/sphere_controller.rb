30a292b7623beed6dd4ff04296b37b28f1430040
David Dever <david.dever@cox.net>
2014-11-25 15:16:24 -0800

    Spheres index and show pages working and linked

--- app/controllers/sphere_controller.rb
+++ app/controllers/sphere_controller.rb
@@ -5,6 +5,5 @@ class SphereController < ApplicationController
 
   def show
     @sphere = Sphere.find(params[:id])
-    @historical_figure = HistoricalFigureSphere.where(@sphere.id == HistoricalFigureSphere.sphere_id)
   end
 end
